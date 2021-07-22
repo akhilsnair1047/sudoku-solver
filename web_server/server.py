8dc3c1e6c81e420591edf62a34d67e554765da77
RPSingh0 <rpalsingh715@gmail.com>
2021-07-22 13:28:49 +0530

    created

---
+++ web_server/server.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from werkzeug.utils import redirect
+from t import solve
+app = Flask(__name__)
+
+
+@app.route('/', methods=["POST", "GET"])
+def hello_world():
+    if request.method == "POST":
+        mat = request.form.to_dict()
+        r = [[int(mat[str(x)]) for x in range(j, j+9)]
+             for j in range(1, 82, 9)]
+        # print(r)
+        r = solve(r)
+
+        # for i in range(9):
+        #     for j in range(9):
+        #         print(r[i][j], end=" ")
+        #     print()
+        return render_template('new.html', r=r)
+    if request.method == "GET":
+        return render_template('home.html')
